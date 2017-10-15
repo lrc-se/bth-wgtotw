21d2cf74fb2f41bf504b4b4371847bea9fa297eb
Kalle Brisland <kabc16@student.bth.se>
2017-10-15 19:36:55 +0200

    Bugfix

--- view/user/profile.php
+++ view/user/profile.php
@@ -1,7 +1,7 @@
 <h1><?= esc($user->username) ?></h1>
 <?php $this->renderView('default/msgs'); ?>
 
-<?php if ($user->id == $curUser->id) : ?>
+<?php if ($curUser && $user->id == $curUser->id) : ?>
 <p>
     <a class="btn" href="<?= $this->url('user/edit/' . $user->id) ?>">Redigera profil</a>
     <a class="btn btn-2" href="<?= $this->url('user/logout') ?>">Logga ut</a>
