bab1f74e25059fa86cb15add91f52a7681ab1647
Kalle Brisland <kabc16@student.bth.se>
2017-10-18 20:37:40 +0200

    Added active user display to start page

--- src/Controllers/DefaultController.php
+++ src/Controllers/DefaultController.php
@@ -16,8 +16,20 @@ class DefaultController extends BaseController
     public function index()
     {
         $questions = $this->di->post->useSoft()->getAll('question', 'published DESC');
+        $activeUsers = $this->di->db->connect()
+            ->select('u.*, COUNT(p.id) AS numPosts')
+            ->from($this->di->repository->posts->getCollectionName() . ' AS p')
+            ->join($this->di->repository->users->getCollectionName() . ' AS u', 'p.userId = u.id AND u.deleted IS NULL')
+            ->where('p.deleted IS NULL')
+            ->groupBy('p.userId')
+            ->orderBy('numPosts DESC')
+            ->limit(5)
+            ->execute()
+            ->fetchAll();
+        
         return $this->di->common->renderMain('start', [
-            'questions' => $questions
+            'questions' => $questions,
+            'activeUsers' => $activeUsers
         ], 'Start');
     }
 }
--- view/start.php
+++ view/start.php
@@ -3,3 +3,12 @@
 
 <h2>Senaste frågorna</h2>
 <?php $this->renderView('question/index', ['questions' => $questions, 'hideTitle' => true]); ?>
+
+<h2>Mest aktiva användare</h2>
+<ul>
+<?php foreach ($activeUsers as $user) : ?>
+    <li>
+        <a href="<?= $this->url('user/' . $user->id) ?>"><?= esc($user->username) ?></a> (<?= $user->numPosts ?> inlägg)
+    </li>
+<?php endforeach; ?>
+</ul>
