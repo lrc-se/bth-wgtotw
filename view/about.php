4fd7a792af05f937e4c378491bdc2df366d4f4c2
Kalle Brisland <kabc16@student.bth.se>
2017-10-22 15:27:56 +0200

    Updated about page

--- view/about.php
+++ view/about.php
@@ -1,13 +1,22 @@
 <h1>Om webb&shy;platsen</h1>
 <p>
-    Denna webbplats är en del i kursen <strong>Webbaserade ramverk 1</strong> i distansprogrammet <em>Webbprogrammering</em> på <a href="http://www.bth.se/">Blekinge Tekniska Högskola</a>. 
-    Kursen handlar om "kodstrukturer med ramverk, designmönster och återanvändbara moduler samt tekniker för automatiserad testning och <em>continuous integration</em> i en fullstack-webbmiljö".
+    Här på WGTOTW:s tjänst <strong>Allt om sci-fi</strong> kan du få reda på, ja just det, allt om sci-fi, denna häftigaste av genrer! 
+    Skapa ett konto, ställ din fråga och delta i allehanda diskussioner om allehanda företeelser inom SF-världen. 
+    Bygg upp ditt rykte genom att skriva många och bra frågor, svar och kommentarer, så att andra medlemmar röstar upp dina inlägg – 
+    och utnyttja möjligheten att påverka rangordningen genom att rösta själv. Alla texter skrivs med Markdown, så du har stor frihet att formatera dina inlägg.
+</p>
+<p>
+    Så, vad väntar du på? <a href="<?= $this->url('user/create') ?>">Gå med</a> idag! Vi bits inte, tummis.
 </p>
 <div class="spacer"></div>
-<h5>Källkod</h5>
-<ul>
-    <li><a href="https://github.com/lrc-se/bth-anax">Webbplatsen på GitHub</a></li>
-    <li><a href="https://github.com/lrc-se/bth-anax-repository"><em>Repository</em>-modulen på GitHub</a></li>
-    <li><a href="https://packagist.org/packages/lrc-se/anax-repository"><em>Repository</em>-modulen på Packagist</a></li>
-    <li><a href="https://github.com/canax">Anax på GitHub</a></li>
-</ul>
+<hr>
+<div class="spacer"></div>
+<p>
+    Webbplatsen är skapad av <a href="mailto:kabc16@student.bth.se">Kalle Brisland</a> som ett avslutande projekt i kursen <a href="https://dbwebb.se/ramverk1">Webbaserade ramverk 1</a> 
+    på Blekinge Tekniska Högskola och <a href="https://github.com/lrc-se/bth-wgtotw">finns på GitHub</a>.
+</p>
+<div class="spacer"></div>
+<p class="center">
+    <img src="<?= $this->asset('img/enterprise.jpg') ?>" alt="NCC-1701-D"><br>
+    <span><em><a href="https://commons.wikimedia.org/wiki/File:Enterprise_(869263349).jpg">Rob Young</a></em></span>
+</p>
