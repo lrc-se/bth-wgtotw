82a37cf10217d53372bbf9a81bf1c38d1c8cc778
Kalle Brisland <kabc16@student.bth.se>
2017-10-23 19:56:38 +0200

    Updated start view

--- view/start.php
+++ view/start.php
@@ -6,9 +6,10 @@
     <div class="column-2">
         <h2>Mest aktiva användare</h2>
         <ul class="post-list user-list">
-<?php foreach ($activeUsers as $user) : ?>
-<?php   $user2 = new \WGTOTW\Models\User(); ?>
-<?php   $user2->email = $user->email; ?>
+<?php if (!empty($activeUsers)) : ?>
+<?php   foreach ($activeUsers as $user) : ?>
+<?php       $user2 = new \WGTOTW\Models\User(); ?>
+<?php       $user2->email = $user->email; ?>
             <li>
                 <span class="post-title">
                     <a href="<?= $this->url('user/' . $user->id) ?>"><img src="<?= $user2->getGravatar(25) ?>" alt=""></a>
@@ -16,19 +17,26 @@
                 </span>
                 <span><span class="icon-message" title="Inlägg"></span> <?= $user->numPosts ?></span>
             </li>
-<?php endforeach; ?>
+<?php   endforeach; ?>
+<?php else : ?>
+        <p><em>Inga användare att visa</em></p>
+<?php endif; ?>
         </ul>
     </div>
     <div class="column-2">
         <h2>Mest använda taggar</h2>
         <ul class="post-list tag-list">
-<?php foreach ($activeTags as $tag) : ?>
+<?php if (!empty($activeTags)) : ?>
+<?php   foreach ($activeTags as $tag) : ?>
             <li>
                 <span class="post-type"><span class="icon-tag"></span></span>
                 <span class="post-title"><a href="<?= $this->url('tag/' . urlencode($tag->name)) ?>"><?= esc($tag->name) ?></a></span>
                 <span class="nowrap"><span class="icon-question" title="Frågor"></span> <?= $tag->numPosts ?></span>
             </li>
-<?php endforeach; ?>
+<?php   endforeach; ?>
+<?php else : ?>
+        <p><em>Inga taggar att visa</em></p>
+<?php endif; ?>
         </ul>
     </div>
 </div>
